9f619f76d5a7f56f36af7547b7e4c4bc2c71989c
Yevheniia1990 <90608187+Yevheniia1990@users.noreply.github.com>
2021-12-19 22:44:52 +0100

    Update javascript.js

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -73,8 +73,96 @@ $(document).ready(function(){
   });
   $('.item').imagefill();
 
-   var mydata = JSON.stringify(data);
-console.log(mydata);
+// json news parse
+   var items = JSON.parse(data);
+
+   for (var i = 0; i <= items.length - 1; i++) {
+
+       //values
+       var item_img = items[i]['img-of-news'];
+       var category = items[i]['category'];
+       var title = items[i]['title'];
+       var text = items[i]['text'];
+       var link = items[i]['link'];
+
+       //elements
+       var elem = document.querySelector('.section-news-container');
+
+       var main_elem = document.createElement("div");
+       main_elem.className = "section-news-container__item";
+       elem.append(main_elem);
+       if (i % 2 == 1) {
+            main_elem.className = "section-news-container__item section-news-container__item-rev";   
+        }
+
+       var container = document.createElement("div");
+       container.className = 'container';
+       main_elem.append(container);
+
+       var row = document.createElement("div");
+       row.className = 'row';
+       container.append(row);
+
+       var col_6 = document.createElement("div");
+       col_6.className = 'col-6';
+       row.append(col_6);
+
+       var section_news_image = document.createElement("div");
+       section_news_image.className = 'section-news-image';
+       col_6.append(section_news_image);
+
+       var image = document.createElement("img");
+       image.className = 'image';
+       section_news_image.append(image);
+       image.src = item_img;
+
+       var overlay = document.createElement("div");
+       overlay.className = 'overlay';
+       section_news_image.append(overlay);
+
+       var more = document.createElement("a");
+       more.className = 'text';
+       overlay.append(more);
+       more.innerText = 'leer más';
+       more.href = link;
+
+
+
+       var col_6_2 = document.createElement("div");
+       col_6_2.className = 'col-6';
+       row.append(col_6_2);
+
+       var col_text = document.createElement("div");
+       col_text.className = 'col-text';
+       col_6_2.append(col_text);
+
+       var section_news_text_h4 = document.createElement("div");
+       section_news_text_h4.className = 'section-news-text-h4';
+       col_text.append(section_news_text_h4);
+       section_news_text_h4.innerText = category;
+
+       var section_news_text_h3 = document.createElement("div");
+       section_news_text_h3.className = 'section-news-text-h3';
+       col_text.append(section_news_text_h3);
+       section_news_text_h3.innerText = title;
+
+       var section_news_line = document.createElement("hr");
+       section_news_line.className = 'section-news-line';
+       col_text.append(section_news_line);
+
+       var section_news_text = document.createElement("p");
+       section_news_text.className = 'section-news-text';
+       col_text.append(section_news_text);
+       section_news_text.innerText = text;
+
+       var more2 = document.createElement("a");
+       more2.className = 'button-border';
+       col_text.append(more2);
+       more2.innerText = 'leer más';
+       more2.href = link;
+   }
+//end json news parse
+
 
 
 });
